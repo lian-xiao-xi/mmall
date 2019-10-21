514decce2cc6e6046caa94007fd8b3c598079762
lian-xiao-xi <13253641328@163.com>
2019-10-21 19:39:57 +0800

    这个类为工具类，某个方法抛出异常之后，其他用到这个方法的地方都要抛出异常，这样的话改动较大。所以撤销上次对这个类的修改。

--- src/main/java/com/mmall/util/MD5Util.java
+++ src/main/java/com/mmall/util/MD5Util.java
@@ -1,10 +1,6 @@
 package com.mmall.util;
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MD5Util {
   private static String byteArrayToHexString(byte b[]) {
@@ -36,22 +32,26 @@ public class MD5Util {
    * @param charsetname
    * @return
    */
-  private static String MD5Encode(String origin, String charsetname) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-    String resultString = new String(origin);
-    MessageDigest md = MessageDigest.getInstance("MD5");
-    if (StringUtils.isBlank(charsetname))
-      resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
-    else
-      resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+  private static String MD5Encode(String origin, String charsetname) {
+    String resultString = null;
+    try {
+      resultString = new String(origin);
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      if (charsetname == null || "".equals(charsetname))
+        resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+      else
+        resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+    } catch (Exception exception) {
+    }
     return resultString.toUpperCase();
   }
   
-  public static String MD5EncodeUtf8(String origin) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+  public static String MD5EncodeUtf8(String origin) {
     origin = origin + PropertiesUtil.getProperty("password.salt", "");
     return MD5Encode(origin, "utf-8");
   }
-
-
+  
+  
   private static final String hexDigits[] = {"0", "1", "2", "3", "4", "5",
     "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
   
