60889faf443ca9b7f03bcd1e776b817a91efeb9d
lian-xiao-xi <13253641328@163.com>
2019-12-21 16:44:27 +0800

    feat: 基于java类对项目进行配置

--- src/main/java/com/mmall/config/ApplicationContextConfig.java
+++ src/main/java/com/mmall/config/ApplicationContextConfig.java
@@ -1,4 +1,13 @@
 package com.mmall.config;
 
+import org.springframework.context.annotation.*;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "com.mmall")
+@EnableAspectJAutoProxy // spring-aop, 启用 AspectJ 自动代理
+@EnableTransactionManagement
+@Import({ApplicationDatasource.class})
 public class ApplicationContextConfig {
+
 }
---
+++ src/main/java/com/mmall/config/ApplicationDatasource.java
@@ -0,0 +1,95 @@
+package com.mmall.config;
+
+import com.github.pagehelper.PageHelper;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ibatis.plugin.Interceptor;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource(value = "classpath:datasource.properties", ignoreResourceNotFound = true)
+public class ApplicationDatasource {
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+    PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+    placeholderConfigurer.setFileEncoding("UTF-8");
+//    placeholderConfigurer.setLocation(new ClassPathResource("datasource.properties"));
+//    placeholderConfigurer.setIgnoreResourceNotFound(true);
+    return placeholderConfigurer;
+  }
+
+  @Bean
+  public BasicDataSource dataSource(
+      @Value("${db.driverClassName}") String driverClassName,
+      @Value("${db.url}") String url,
+      @Value("${db.username}") String username,
+      @Value("${db.password}") String password,
+      @Value("${db.initialSize}") Integer initialSize,
+      @Value("${db.maxActive}") Integer maxActive,
+      @Value("${db.maxIdle}") Integer maxIdle,
+      @Value("${db.minIdle}") Integer minIdle,
+      @Value("${db.maxWait}") Integer maxWait,
+      @Value("${db.defaultAutoCommit}") Boolean defaultAutoCommit
+  ) {
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setDriverClassName(driverClassName);
+    basicDataSource.setUrl(url);
+    basicDataSource.setUsername(username);
+    basicDataSource.setPassword(password);
+    basicDataSource.setInitialSize(initialSize);
+    basicDataSource.setMaxActive(maxActive);
+    basicDataSource.setMaxIdle(maxIdle);
+    basicDataSource.setMinIdle(minIdle);
+    basicDataSource.setMaxWait(maxWait);
+    basicDataSource.setDefaultAutoCommit(defaultAutoCommit);
+    basicDataSource.setTimeBetweenEvictionRunsMillis(40000L);
+    basicDataSource.setTestWhileIdle(true);
+    basicDataSource.setValidationQuery("SELECT 1 FROM dual");
+    return basicDataSource;
+  }
+
+  @Bean
+  public PageHelper pageHelper() {
+    PageHelper pageHelper = new PageHelper();
+    Properties properties = new Properties();
+    properties.setProperty("dialect", "mysql");
+    pageHelper.setProperties(properties);
+    return pageHelper;
+  }
+
+  @Bean
+  public MapperScannerConfigurer mapperScannerConfigurer() {
+    MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+    scannerConfigurer.setBasePackage("com.mmall.dao");
+    return scannerConfigurer;
+  }
+
+  @Bean
+  public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource, PageHelper pageHelper) {
+    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    sqlSessionFactoryBean.setDataSource(dataSource);
+    sqlSessionFactoryBean.setMapperLocations(new ClassPathResource[] {new ClassPathResource("/mappers/*Mapper.xml")});
+    sqlSessionFactoryBean.setPlugins(new Interceptor[]{pageHelper});
+    return sqlSessionFactoryBean;
+  }
+
+  @Bean
+  public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+    DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+    dataSourceTransactionManager.setRollbackOnCommitFailure(true);
+    dataSourceTransactionManager.setDataSource(dataSource);
+    return dataSourceTransactionManager;
+  }
+
+
+}
